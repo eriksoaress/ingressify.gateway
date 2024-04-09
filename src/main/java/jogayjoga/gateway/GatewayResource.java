b904491eb0198903c81bee1af6e8d3811bdb025b
GustavoAntony <gustavoa9@al.insper.edu.br>
2024-04-08 22:25:22 -0300

    atualizando documentation

--- src/main/java/jogayjoga/gateway/GatewayResource.java
+++ src/main/java/jogayjoga/gateway/GatewayResource.java
@@ -3,10 +3,15 @@ package jogayjoga.gateway;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.Api;
+
 @RestController
+@Api(tags = "Gateway", description = "Endpoints relacionados ao gateway")
 public class GatewayResource {
 
     @GetMapping("/hello")
+    @ApiOperation(value = "hello mundo")
     public String hello() {
         return "Hello from Gateway!";
     }
